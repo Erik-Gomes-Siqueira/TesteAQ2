92685ae264f7ce56cc4528f454ef0bca65c759c8
Erik Gomes Siqueira <67563562+Erik-Gomes-Siqueira@users.noreply.github.com>
2021-08-08 13:27:45 -0300

    Update App.js
    Melhor forma de fazer uma requisição

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Image, ScrollView } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
 import api from './src/services/api'
 
 
@@ -9,40 +9,39 @@ const App = () => {
   const [isAuth, setIsAuth] = useState(false)
   const [isLoading, setIsLoading] = useState(true)
   
-  const user = async () => {
-    await api.get('/users/Erik-Gomes-Siqueira')
-    .then((response)=> {
+  const user = async ()=>{
+    try {
+      const response = await api.get('/users/joaomarcos-exe');
       const{ avatar_url, name, bio } = response.data
       setUserData({
         avatar_url,
         name,
         bio
       })
-      setIsAuth(true)
+        setIsAuth(true)
+        setIsLoading(false)
+    } catch(err) {
+      setUserData({error: err})
+      setIsAuth(false)
       setIsLoading(false)
-                })
-                .catch((err) =>{
-                  setUserData({error: err})
-                  setIsAuth(false)
-                  setIsLoading(false)
-              })
-              
-            }
+    }
+  }
+
   useEffect(() => user(),[])
   if(isLoading){
     return <Text>Aguarde...</Text>
   }
   else{
     return(
-      <View>
+      <View style={styles.container}>
         {isAuth ?
               <>
-                <Image source={{uri: userData.avatar_url}}/>
-                <Text>{userData.name}</Text>
-                <Text>{userData.bio}</Text>
+                <Image source={{uri: userData.avatar_url}} style={styles.image}/>
+                <Text style={styles.name}>{userData.name}</Text>
+                <Text style={styles.bio}>{userData.bio}</Text>
               </>
             :
-              <Text>{`${userData.error}`}</Text>
+              <Text style={styles.error}>{`${userData.error}`}</Text>
             
         }
       </View>
@@ -50,5 +49,29 @@ const App = () => {
   }
   
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    alignItems: 'center',
+    padding: 20
+  },
+  image:{
+    width: 300, 
+    height: 300,
+    borderRadius: 150
+  },
+  name:{
+    fontSize: 25,
+    fontWeight: 'bold'
+  },
+  bio:{
+    fontSize: 18,
+  },
+  error:{
+    fontSize: 25,
+    fontWeight: 'bold'
+  },
+})
 
 export default App;
